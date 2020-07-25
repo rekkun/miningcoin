2dbddab4311d42158086fb76a295241930269ff1
rekkun1-xkx-me <altrekdv8@gmail.com>
2020-07-25 19:38:33 +0000

    test

---
+++ main.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+function Work() {
+    (async () => {
+        await page.goto('http://gudproxyforme.epizy.com/index.html');
+        console.log('OK')
+    })
+}
+
+(async () => {
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await page.goto('http://gudproxyforme.epizy.com/index.html');
+    console.log('OK')
+    setInterval(Work,600000) 
+})()
