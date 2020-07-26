a755d0e564bcd784e1422b314d5063125a7aca2b
Rek <57406598+rekkun1-xkx-me@users.noreply.github.com>
2020-07-26 15:18:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,26 @@
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-if (cluster.isMaster) {
-	console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
-	for (let i = 0; i < numCPUs; i++) {
-		cluster.fork();
-	}
-
-	  cluster.on('exit', (worker, code, signal) => {
-		console.log(`worker ${worker.process.pid} died`);
-	  });
-	} else {
-		const puppeteer = require('puppeteer');
-
-		function Work() {
-			(async () => {
-				await page.goto('http://gudproxyforme.epizy.com/index.html');
-				console.log('OK')
-			})
-		}
-
-		(async () => {
-			const puppeteer = require('puppeteer');
-			const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-			const page = await browser.newPage();
-			await page.goto('http://gudproxyforme.epizy.com/index.html');
-			console.log('OK')
-			setInterval(Work,600000) 
-		})()
-
-	console.log(`Worker ${process.pid} started`);
+//function Work() {
+//	(async () => {
+//		await page.goto('http://gudproxyforme.epizy.com/index.html');
+//		console.log('OK')
+//	})
+//}
+const { Cluster } = require('puppeteer-cluster');
+ 
+(async () => {
+	function Work() {
+	(async () => {
+		await page.goto('http://gudproxyforme.epizy.com/index.html');
+		console.log('OK')
+	})
 }
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_BROWSER,
+    maxConcurrency: 8,
+  });
+ 
+  await cluster.task(async ({ page, data: url }) => {
+	await page.goto('http://gudproxyforme.epizy.com/index.html');
+	console.log('OK')
+	setInterval(Work,600000) 
+  });
+})();
